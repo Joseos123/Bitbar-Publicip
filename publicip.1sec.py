030cc44ace938013c1d6b373cb81d3c37f6f7761
Joshua Tsao <joshuahj.tsao@gmail.com>
2020-11-22 12:44:49 +0800

    Add files via upload

---
+++ publicip.1sec.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env PYTHONIOENCODING=UTF-8 /usr/local/bin/python3
+
+import publicip
+
+publicip.get()
+print("---")
+print("ipinfo.io" + " | href=""https://ipinfo.io/")
+print("ipleak.net" + " | href=""https://ipleak.net")
