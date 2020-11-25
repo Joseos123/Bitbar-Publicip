91438bafb17ef95d6e2fd218e298dd0f03d14b52
Joshua Tsao <joshuahj.tsao@gmail.com>
2020-11-25 19:22:55 +0800

    python library publicip just killed itself

--- publicip.1sec.py
+++ publicip.1sec.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env PYTHONIOENCODING=UTF-8 /usr/local/bin/python3
 
-import publicip
+from requests import get
 
-publicip.get()
+ip = get('https://api.ipify.org').text
+print('{}'.format(ip))
 print("---")
 print("ipinfo.io" + " | href=""https://ipinfo.io/")
 print("ipleak.net" + " | href=""https://ipleak.net")
